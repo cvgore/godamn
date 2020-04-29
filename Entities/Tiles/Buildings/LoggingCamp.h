ec59f4c6b5d88f91860688ec066ce6bfd541278c
Grzegorz Faber <grzesiufab@gmail.com>
2020-04-29 16:29:39 +0200

    repaired GUID in mine and loggingcamp

--- Entities/Tiles/Buildings/LoggingCamp.h
+++ Entities/Tiles/Buildings/LoggingCamp.h
@@ -14,7 +14,7 @@ namespace Godamn
 
 	public:
 		// {64D85950-96D7-4822-BDBF-69B3025AE277}
-		static const GUID entity_id ={
+		inline static constexpr GUID entity_id ={
 			0x64d85950, 0x96d7, 0x4822, { 0xbd, 0xbf, 0x69, 0xb3, 0x2, 0x5a, 0xe2, 0x77 }
 		};
 
--- Entities/Tiles/Buildings/Mine.h
+++ Entities/Tiles/Buildings/Mine.h
@@ -14,7 +14,7 @@ namespace Godamn
 
 	public:
 		// {0157E010-B827-4E96-84B4-5EA09B95BD65}
-		static const GUID entity_id ={
+		inline static constexpr GUID entity_id ={
 			0x157e010, 0xb827, 0x4e96, { 0x84, 0xb4, 0x5e, 0xa0, 0x9b, 0x95, 0xbd, 0x65 }
 		};
 
