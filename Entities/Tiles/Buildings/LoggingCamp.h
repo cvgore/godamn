c39593c01c6305027e2ffc8e913c975a44f47049
Kacper Palka <kacper@cvgo.re>
2020-04-30 19:52:02 +0200

    Fix styles

--- Entities/Tiles/Buildings/LoggingCamp.h
+++ Entities/Tiles/Buildings/LoggingCamp.h
@@ -14,7 +14,6 @@ namespace Godamn
 
 	public:
 		// {64D85950-96D7-4822-BDBF-69B3025AE277}
-
 		inline static constexpr GUID entity_id ={
 			0x64d85950, 0x96d7, 0x4822, { 0xbd, 0xbf, 0x69, 0xb3, 0x2, 0x5a, 0xe2, 0x77 }
 		};
